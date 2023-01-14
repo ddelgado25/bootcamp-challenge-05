b43f46e4ded76807cb4ca9dc55b12d082b8822b1
ddelgado25 <daviddelgado2515@gmail.com>
2023-01-13 19:13:47 -0500

    added time block function for past, present, and future

--- Develop/script.js
+++ Develop/script.js
@@ -11,14 +11,35 @@ $(document).ready ($(function(){
     var hour = $(this).parent().attr("id");
     var hourlyEvents = $(this).siblings(".description").val();
     localStorage.setItem(hour,hourlyEvents);
+    console.log(hour);
+    console.log(hourlyEvents);
   })
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+
+  var presentHour = dayjs().hour();
+  console.log(presentHour);
+
+  $(".time-block").each(function(){
+    if (presentHour === dayjs().hour()){
+      $(this).addClass("present");
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+    
+    }
+    if (presentHour < dayjs().hour()){
+      $(this).addClass("past");
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+    
+    }
+    if (presentHour > dayjs().hour()){
+      $(this).addClass("future");
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+
+    }
+
+  })
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
